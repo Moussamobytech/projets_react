30454368861cb43ab45c483491fd52772ce37da9
Moussamobytech <144075779+Moussamobytech@users.noreply.github.com>
2023-11-04 13:59:15 +0100

    Add files via upload

---
+++ src/comp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MonComposant() {
+  return (
+    <div>
+      <h1>Bienvenue dans mon premier composant React !</h1>
+      <p>C'est génial de commencer à coder avec ReactJS.</p>
+    </div>
+  );
+}
+
+export default MonComposant;
